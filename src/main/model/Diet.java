58bc83009c8b76d61cf9eeb48db640cfba51302c
jg0914 <z6s0i@students.cs.ubc.ca>
2023-02-24 14:16:30 -0800

    Added In Classes

---
+++ src/main/model/Diet.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Diet {
+    ArrayList<Nutrient> nutrients = new ArrayList<Nutrient>();
+}
--- src/test/model/PersonTest.java
+++ src/test/model/PersonTest.java
@@ -1,5 +1,5 @@
 package model;
 
-public class MyModel {
+class PersonTest {
     // delete or rename this class!
 }
